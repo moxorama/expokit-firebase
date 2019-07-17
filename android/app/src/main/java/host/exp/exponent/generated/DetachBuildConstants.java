cb02c2ccd0592cb14e5080ed87a37a8b5fa93de8
George Wizeman <Wizeman@WizeMac.local>
2019-07-17 17:06:16 +0300

    Initial import

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp5cdf2d9f1aec4b9ea86ab4396ca53132://192.168.31.83:80";
+
+}
